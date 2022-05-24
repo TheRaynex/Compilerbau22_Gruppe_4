0f90dc998b4b8e3abdd17cf7943f5c8deaffc7b9
Markus Eble <markus.eble@online.de>
2022-05-24 23:29:02 +0200

    fix ExpressionParserMain

--- ExpressionParserMain.java
+++ ExpressionParserMain.java
@@ -5,8 +5,8 @@ public class ExpressionParserMain {
     public static void main(String[] args) throws Exception {
         compiler.Lexer lexer = new compiler.Lexer();
         compiler.Parser exprParser = new compiler.Parser(lexer);
-        System.out.println(expr.eval());
         compiler.ast.ASTExprNode expr = exprParser.parseExpression("6 & 3");
+        System.out.println(expr.eval());
         OutputStreamWriter outStream = new OutputStreamWriter(System.out, "UTF-8");
         expr.print(outStream, "");
         outStream.flush();
