2224ad0bf688348dc2402621d519330db9890c40
TimCzks <t.handy@gmx.de>
2022-06-02 13:04:37 +0200

    TestCommit

---
+++ compiler/ast/ASTWhileStmtNode.java
@@ -0,0 +1,17 @@
+package compiler.ast;
+
+import java.io.OutputStreamWriter;
+
+public class ASTWhileStmtNode extends ASTStmtNode {
+
+	@Override
+	public void print(OutputStreamWriter outStream, String indent) throws Exception {
+
+	}
+
+	@Override
+	public void execute() {
+
+	}
+
+}
