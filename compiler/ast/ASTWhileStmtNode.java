f5cffbcf0334bb65f8fbcb51e1b4269f86641cfa
TimCzks <t.handy@gmx.de>
2022-06-03 21:30:31 +0200

    Added execute() and halfway finished codegen() method

--- compiler/ast/ASTWhileStmtNode.java
+++ compiler/ast/ASTWhileStmtNode.java
@@ -2,6 +2,8 @@ package compiler.ast;
 
 import java.io.OutputStreamWriter;
 
+import compiler.CompileEnv;
+
 public class ASTWhileStmtNode extends ASTStmtNode {
 	
 	private ASTExprNode exprNode;
@@ -14,12 +16,30 @@ public class ASTWhileStmtNode extends ASTStmtNode {
 
 	@Override
 	public void print(OutputStreamWriter outStream, String indent) throws Exception {
-
+		outStream.append(indent);
+		outStream.append("WHILE\n");
+		String childIndent = indent + "  ";
+		exprNode.print(outStream, childIndent);
+        blockstmt.print(outStream, childIndent);
 	}
 
 	@Override
 	public void execute() {
-
+		while(exprNode.eval() > 0) {
+			blockstmt.execute();
+		}
+	}
+	
+	@Override
+	public void codegen(CompileEnv env) {
+		 // trigger codegen for all child nodes
+        this.exprNode.codegen(env);
+        compiler.InstrIntf instrToEval = this.exprNode.getInstr();
+        this.blockstmt.codegen(env);
+        compiler.InstrIntf instrToExecute = this.blockstmt.getInstr();
+        
+        // create instruction object
+        // add instruction to current code block (?)
 	}
 
 }
